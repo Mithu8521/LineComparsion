80464e1706e51a455ab3ab0a05894c2008492677
Mithu8521 <mithileshkr8521@gmail.com>
2021-08-21 23:37:41 +0530

    Find Length

--- LineComparsion.java
+++ LineComparsion.java
@@ -1,5 +1,10 @@
 public class LineComparsion {
     public static void main(String[] args) {
-        System.out.println("Welcome To Line Comparsion Computation");
+        int x1 = 2;
+        int y1 = 5;
+        int x2 = 4;
+        int y2 = 8;
+        double result = Math.sqrt((x2 - x1) + (y2 - y1));
+        System.out.println("length of a line" + '\n' + result);
     }
 }
